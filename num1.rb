0ea3a2c86ef878296c5b804ecd853f1a92433f54
nexxrt <62123304+nexxrt@users.noreply.github.com>
2023-03-27 12:35:23 +0300

    Update num1.rb

--- num1.rb
+++ num1.rb
@@ -5,8 +5,8 @@ print "Рост - "
 puts "Введите ваш рост"
 height = gets.to_i
 weight = (height - 110) * 1.15 
-	if weight >= 0
-		puts "#{name.capitalize}, ваш оптимальный вес #{weight}"
-	else
-		puts "Ваш вес уже оптимальный"
-	end
+  if weight >= 0
+    puts "#{name.capitalize}, ваш оптимальный вес #{weight}"
+  else
+    puts "Ваш вес уже оптимальный"
+  end
